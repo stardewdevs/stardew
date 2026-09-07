f0ba71268d33124dfdb47184a9358c7a17136ba5
Frostre_ <n9043395@gmail.com>
2026-09-07 15:21:27 +0200

    Reorganize project includes in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,9 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Stardew"
 include(":stardew-app")
-
-//This later
-// include(":stardew-emulator")
-// include(":stardew-view")
-// include(":stardew-mpkg")
-// include(":stardew-shared")
