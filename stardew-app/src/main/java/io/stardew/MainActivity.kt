a130f3318b822931c9859c23148058caac8272b1
Frostre_ <n9043395@gmail.com>
2026-09-07 12:04:45 +0200

    Refactor MainActivity to remove StardewNavGraph
    Removed StardewNavGraph from MainActivity and added a placeholder in the Surface.

--- stardew-app/src/main/java/io/stardew/MainActivity.kt
+++ stardew-app/src/main/java/io/stardew/MainActivity.kt
@@ -4,24 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import io.stardew.ui.navigation.StardewNavGraph
 import io.stardew.ui.theme.StardewTheme
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             StardewTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    StardewNavGraph()
+                Surface(modifier = Modifier.fillMaxSize()) {
+                // placeholder
                 }
             }
         }
