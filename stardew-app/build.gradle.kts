1b6956a36fc6fd920b0c8fe6d8753bbbe66802a9
Frostre_ <n9043395@gmail.com>
2026-09-07 11:59:38 +0200

    Update SDK versions to 35 and adjust version name

--- stardew-app/build.gradle.kts
+++ stardew-app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "io.stardew"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.stardew"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1"
     }
 
     buildFeatures {
