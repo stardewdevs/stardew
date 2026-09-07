2642412b232ae733c6ee70017aff96639b698b1f
Frostre_ <n9043395@gmail.com>
2026-09-07 13:21:07 +0200

    Add buildscript and configure repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,25 @@
 plugins {
-    // no plugins
+    // No plugins needed at root level
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
