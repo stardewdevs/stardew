f1931d35fd807aef71f934d1e056aea0105fdf75
Frostre_ <n9043395@gmail.com>
2026-09-07 13:23:28 +0200

    Comment out emulator and view project includes
    Comment out additional project includes for future reference.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "Stardew"
 include(":stardew-app")
+
+//This later
+// include(":stardew-emulator")
+// include(":stardew-view")
+// include(":stardew-mpkg")
+// include(":stardew-shared")
